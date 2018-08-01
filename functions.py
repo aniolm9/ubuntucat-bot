657eb06e56d467df29656fbee1d7612c520d617e
Aniol Marti <aniol@aniolmarti.cat>
2018-08-01 22:43:35 +0200

    Cleaned code adding functions.py

---
+++ functions.py
@@ -0,0 +1,19 @@
+import commands
+import datetime
+
+def avisos(bot):
+    # Timing alarms
+    now = datetime.datetime.now()
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+    weekday = now.weekday()
+    day = now.day
+    #print(hour, minute, second, weekday, day)
+
+    if (hour == 10 and minute == 0 and second == 5 and weekday == 2 and day < 8):
+        commands.avisMati(bot)
+    elif (hour == 21 and minute == 0 and second == 5 and weekday == 2 and day < 8):
+        commands.avisVespre(bot)
+    elif (hour == 21 and minute == 55 and second == 5 and weekday == 2 and day < 8):
+        commands.avisImmediat(bot)
--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # Developed by Xaldiks
 
 import time
-import datetime
 import telepot
+import functions
 import commands
 from telepot.loop import MessageLoop
 
@@ -11,7 +11,7 @@ def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
 
-    print ('Got command: %s' % command)
+    print ('Got command: %s from chat_id: %d' % (command, chat_id))
 
     if (command == "/start"):
         commands.start(bot, chat_id)
@@ -21,28 +21,9 @@ TOKEN = f.readline().rstrip()
 f.close()
 bot = telepot.Bot(TOKEN)
 
-
-
 MessageLoop(bot, handle).run_as_thread()
 print ('I am listening ...')
 
 while 1:
-
-    # Timing alarms
-    now = datetime.datetime.now()
-    hour = now.hour
-    minute = now.minute
-    second = now.second
-    weekday = now.weekday()
-    day = now.day
-    print(hour, minute, second, weekday, day)
-
-    if (hour == 10 and minute == 0 and second == 5 and weekday == 2 and day < 8):
-        commands.avisMati(bot)
-    elif (hour == 21 and minute == 0 and second == 5 and weekday == 2 and day < 8):
-        commands.avisVespre(bot)
-    elif (hour == 21 and minute == 55 and second == 5 and weekday == 2 and day < 8):
-        commands.avisImmediat(bot)
-
-
+    functions.avisos(bot)
     time.sleep(0.8)
